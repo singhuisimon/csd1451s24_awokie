c25186a0441be25e429e0bfcd298d1e0c5c407ef
sherraynechloe <136434521+sherraynechloe@users.noreply.github.com>
2024-02-03 23:13:55 +0800

    update

---
+++ collision.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "enum.h"
+#include "Initialisation.h"
+#include <iostream>
+
+struct position {
+	float x;
+	float y;
+};
+
+struct velocity {
+	float x; //speed and direction of the object, positive is to the right negative is to the left
+	float y; //speed and direction of the object, positive is to the right negative is to the left
+};
+
+struct allcollision {
+	position finalposition();
+	velocity movevelocity();
+	int collisionresult = 0;
+
+};
+
+int collisionResult(int objectId, int objectId2);
+
+allcollision collide(int stationaryId, int movingId, int xVelo, int yVelo, int xPos2, int yPos2, int result, allcollision);
+
